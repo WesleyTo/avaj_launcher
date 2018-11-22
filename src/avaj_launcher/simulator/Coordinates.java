4b205f5f1fe69e4552daca1affdeb7c3d7475fc2
Wesley To <wes_t14@hotmail.com>
2018-11-21 19:21:11 -0800

    added coordinates and weather tower

---
+++ src/avaj_launcher/simulator/Coordinates.java
@@ -0,0 +1,42 @@
+// ************************************************************************** //
+//                                                                            //
+//                                                        :::      ::::::::   //
+//   Coordinates.java                                   :+:      :+:    :+:   //
+//                                                    +:+ +:+         +:+     //
+//   By: wto <marvin@42.fr>                         +#+  +:+       +#+        //
+//                                                +#+#+#+#+#+   +#+           //
+//   Created: 2018/11/21 19:16:15 by wto               #+#    #+#             //
+//   Updated: 2018/11/21 19:16:15 by wto              ###   ########.fr       //
+//                                                                            //
+// ************************************************************************** //
+
+package avaj_launcher.simulator;
+
+public class Coordinates
+{
+	private int longitude;
+	private int latitude;
+	private int height;
+
+	Coordinates(int longitude, int latitude, int height)
+	{
+		this.longitude = longitude;
+		this.latitude = latitude;
+		this.height = height;
+	}
+
+	public int getLongitude()
+	{
+		return this.longitude;
+	}
+
+	public int getLatitude()
+	{
+		return this.latitude;
+	}
+
+	public int getHeight()
+	{
+		returh this.height;
+	}
+}
---
+++ src/avaj_launcher/simulator/WeatherTower.java
@@ -0,0 +1,26 @@
+// ************************************************************************** //
+//                                                                            //
+//                                                        :::      ::::::::   //
+//   WeatherTower.java                                  :+:      :+:    :+:   //
+//                                                    +:+ +:+         +:+     //
+//   By: wto <marvin@42.fr>                         +#+  +:+       +#+        //
+//                                                +#+#+#+#+#+   +#+           //
+//   Created: 2018/11/21 19:14:37 by wto               #+#    #+#             //
+//   Updated: 2018/11/21 19:14:38 by wto              ###   ########.fr       //
+//                                                                            //
+// ************************************************************************** //
+
+package avaj_launcher.simulator;
+
+public class WeatherTower extends Tower
+{
+	public String getWeather(Coordinates coordinates)
+	{
+
+	}
+
+	void changeWeather()
+	{
+		
+	}
+}
