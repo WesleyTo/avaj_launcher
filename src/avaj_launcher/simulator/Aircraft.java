6c6008572ea304e46e3de33d78f0244cca51340a
Wesley To <wes_t14@hotmail.com>
2018-11-23 01:05:03 -0800

    updated aircrafts to handle weather changes and to log messages

--- src/avaj_launcher/simulator/Aircraft.java
+++ src/avaj_launcher/simulator/Aircraft.java
@@ -14,11 +14,38 @@ package avaj_launcher.simulator;
 
 public abstract class Aircraft
 {
+	protected String type;
 	protected long id;
 	protected String name;
 	protected Coordinates coordinates;
 
 	private static long idCounter = 0;
+
+	protected void log(String s)
+	{
+		String msg = String.format(
+			"%s#%s(%d) %s %s",
+			this.type,
+			this.name,
+			this.id,
+			this.coordinates.toString(),
+			s
+		);
+		System.out.println(msg);
+	}
+
+	protected void land(String s)
+	{
+
+		String msg = String.format(
+			"%s#%s(%d) %s caused the craft to land",
+			this.type,
+			this.name,
+			this.id,
+			s
+		);
+		System.out.println(msg);
+	}
 
 	protected Aircraft(String name, Coordinates coordinates)
 	{
--- src/avaj_launcher/simulator/Baloon.java
+++ src/avaj_launcher/simulator/Baloon.java
@@ -15,16 +15,50 @@ package avaj_launcher.simulator;
 public class Baloon extends Aircraft implements Flyable
 {
 	private WeatherTower weatherTower;
+	private static final int[][] update = {
+		{2, 0, 4},
+		{0, 0, -3},
+		{0, 0, -2},
+		{0, 0, -15}
+	};
 	
 	Baloon(String name, Coordinates coordinates)
 	{
 		super(name, coordinates);
+		this.type = "Baloon";
 	}
 
 	@Override
 	public void updateConditions()
 	{
 		String weather = this.weatherTower.getWeather(this.coordinates);
+		int i = 3;
+		if (weather == WeatherProvider.sun)
+		{
+			i = 0;
+		}
+		else if (weather == WeatherProvider.fog)
+		{
+			i = 1;
+		}
+		else if (weather == WeatherProvider.rain)
+		{
+			i = 2;
+		}
+		this.coordinates = new Coordinates(
+			this.coordinates.getLongitude() + update[i][0],
+			this.coordinates.getLatitude() + update[i][1],
+			this.coordinates.getHeight() + update[i][2]
+		);
+		if (this.coordinates.getHeight() <= 0)
+		{
+			this.weatherTower.unregister(this);
+			this.land(WeatherProvider.getWeatherTypes()[i]);
+		}
+		else
+		{
+			this.log(WeatherProvider.getWeatherTypes()[i]);
+		}
 	}
 
 	@Override
