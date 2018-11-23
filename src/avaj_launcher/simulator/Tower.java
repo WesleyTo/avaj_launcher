09d831980e3977bd4b3c39b27a7226ef390f48fc
Wesley To <wes_t14@hotmail.com>
2018-11-23 01:44:25 -0800

    added logging to tower register and unregister methods

--- src/avaj_launcher/simulator/Tower.java
+++ src/avaj_launcher/simulator/Tower.java
@@ -12,21 +12,52 @@
 
 package avaj_launcher.simulator;
 
+import avaj_launcher.util.Logger;
 import java.util.HashSet;
+import java.io.IOException;
 
 public abstract class Tower
 {
 	private HashSet<Flyable> observers = new HashSet<Flyable>();
 	private HashSet<Flyable> ignored = new HashSet<Flyable>();
+	private static final Logger log = Logger.getLogger();
 
 	public void register(Flyable flyable)
 	{
 		observers.add(flyable);
+		String msg = String.format(
+			"Tower says: %s registered",
+			flyable.toString()
+		);
+		System.out.println(msg);
+		try
+		{
+			log.write(msg);
+		}
+		catch (IOException e)
+		{
+			System.out.println(e);
+			System.exit(1);
+		}
 	}
 
 	public void unregister(Flyable flyable)
 	{
 		ignored.add(flyable);
+		String msg = String.format(
+			"Tower says: %s unregistered",
+			flyable.toString()
+		);
+		System.out.println(msg);
+		try
+		{
+			log.write(msg);
+		}
+		catch (IOException e)
+		{
+			System.out.println(e);
+			System.exit(1);
+		}
 	}
 
 	public int numObservers()
